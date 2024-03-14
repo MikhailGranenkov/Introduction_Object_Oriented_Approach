23d5e8f63a3cb7fbe3b807fbec78d01f729e3e9d
Mikhail Granenkov <mgran@yandex.ru>
2024-03-14 18:42:11 +0300

    add new

--- Seminars/Seminar2/Task1/ActorBehavior.java
+++
@@ -1,8 +0,0 @@
-package Seminars.Seminar2.Task1;
-
-
-
-public interface ActorBehavior {
-    void makeOrder();
-    void receiveOrder();
-}
--- Seminars/Seminar2/Task1/Human.java
+++ Seminars/Seminar2/Task1/Human.java
@@ -1,33 +1,63 @@
 package Seminars.Seminar2.Task1;
 
 
-public class Human extends Actor implements ActorBehavior {
-    private String job;
+// Задача 1
+// Необходимо реализовать:
+// Класс Human, который должен наследоваться от Actor и реализовывать ActorBehavoir
+// Интерфейс ActorBehavoir, который будет содержать описание возможных действий актора в очереди/магазине
+// Абстрактный класс Actor, который хранит в себе параметры актора, включая состояние готовности сделать заказ и факт получения заказа.
+// Дополнение: для большего понимания, можно сделать методы-геттеры для имени и прочих “персональных данных” abstract
 
-    public Human(String name, String job) {
+
+// Интерфейс описывающий поведение актора
+interface ActorBehaviour {
+    void act(); // Метод, описывающий действие актора
+}
+
+// Абстрактный класс, описывающий актора
+abstract class Actor {
+    private String name;
+    private boolean readyToOrder;
+    private boolean hasOrdered;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isReadyToOrder() {
+        return readyToOrder;
+    }
+
+    public void setReadyToOrder(boolean readyToOrder) {
+        this.readyToOrder = readyToOrder;
+    }
+
+    public boolean hasOrdered() {
+        return hasOrdered;
+    }
+
+    public void setHasOrdered(boolean hasOrdered) {
+        this.hasOrdered = hasOrdered;
+    }
+}
+
+// Класс, представляющий человека
+public class Human extends Actor implements ActorBehaviour {
+    public Human(String name) {
         super(name);
-        this.job = job;
-    }
-
-    public String getJob() {
-        return job;
     }
 
     @Override
-    public void makeOrder() {
-        if (isReadyToOrder()) {
-            System.out.println(getName() + " is making an order.");
+    public void act() {
+        if (isReadyToOrder() && !hasOrdered()) {
+            System.out.println(getName() + " is ready to order.");
+            setHasOrdered(true);
         } else {
-            System.out.println(getName() + " is not ready to make an order.");
-        }
-    }
-
-    @Override
-    public void receiveOrder() {
-        if (isOrderReceived()) {
-            System.out.println(getName() + " has received the order.");
-        } else {
-            System.out.println(getName() + " has not received the order yet.");
+            System.out.println(getName() + " is waiting.");
         }
     }
 }
